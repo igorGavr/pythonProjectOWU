39c47237030622447ac50bc81de44a39f557eff4
igorGavr <ragamuffinrogi@gmail.com>
2022-11-30 20:12:42 +0200

    first

---
+++ stepik/advanced_1.py
@@ -0,0 +1,15 @@
+from math import *
+
+# s = '1234567'
+# n = []
+#
+# while len(s) > 0:
+#     n.append(s[-3:])
+#     print(s[-3:])
+#     s = s[:-3]
+#     print(s[:-3])
+#
+# new_n = n[:: -1]
+# print(*new_n, sep=',')
+#
+# print('{:,}'.format(123456))
