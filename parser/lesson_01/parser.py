56efbff5dfde6dcf80711f3c10efcfbef8d26b88
igorGavr <ragamuffinrogi@gmail.com>
2023-02-07 11:53:14 +0200

    Scraping house.kg

--- parser/lesson_01/parser.py
+++ parser/lesson_01/parser.py
@@ -1,14 +1,33 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
+URL = 'https://www.house.kg/snyat-kvartiru?region=1&town=2&sort_by=upped_at+desc'
 
-file = open('test.html', encoding='utf-8')
 
-html = file.read()
+def get_html(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    return None
 
-soup = BS(html, 'html.parser')
-m = soup.find(class_ = 'content-container').find_all('h1', {'class':'title'})
-print(m)
+def get_post_links(html):
+    links = []
+    soup = BS(html, 'html.parser')
+    container = soup.find('div', {"class": "listings-wrapper"})
+    posts = container.find_all('div', {'class': 'listing'})
+    for post in posts:
+        p = post.find('p', {'class': 'title'}).find('a').get('href')
+        full_link = 'https://www.house.kg' + p
+        # print(full_link)
+        links.append(full_link)
+    return links
 
-for i in m:
-    print(i.text.strip())
+
+def main():
+    html = get_html(URL)
+    links = get_post_links(html)
+    for link in links:
+        detail_html = get_html(link)
+
+if __name__ == '__main__':
+    main()
