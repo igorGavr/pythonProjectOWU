ddd02230a837b62a3acc7c519f977a5f87b86270
igorGavr <ragamuffinrogi@gmail.com>
2023-02-12 23:01:30 +0200

    Scraping house.kg

--- parser/lesson_01/parser.py
+++ parser/lesson_01/parser.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as BS
+from datetime import datetime
+import xlsxwriter
 
 URL = 'https://www.house.kg/snyat-kvartiru?region=1&town=2&sort_by=upped_at+desc'
+array = []
 
 
 def get_html(url):
@@ -19,46 +22,83 @@ def get_post_links(html):
     for post in posts:
         p = post.find('p', {'class': 'title'}).find('a').get('href')
         full_link = 'https://www.house.kg' + p
-        # print(full_link)
         links.append(full_link)
     return links
 
 
 def get_post_data(html):
     soup = BS(html, "html.parser")
-    details_header = soup.find('div', {'class':'details-header'})
-    title = details_header.find('div', {'class':'left'})\
+    details_header = soup.find('div', {'class': 'details-header'})
+    title = details_header.find('div', {'class': 'left'}) \
         .find('h1').text.strip()
-    price = details_header.find('div', {'class':'price-dollar'}).text.strip()
-    address = details_header.find('div', {'class':'address'}).text.strip()
+    price = details_header.find('div', {'class': 'price-dollar'}).text.strip()
+    address = details_header.find('div', {'class': 'address'}).text.strip()
     number = soup.find('div', {'class': 'number'}).text.strip()
-    lat = soup.find('div', {'class': 'map-wrapper'}).find('div', {'id':'map2gis'}).get('data-lat')
-    lon = soup.find('div', {'class': 'map-wrapper'}).find('div', {'id':'map2gis'}).get('data-lon')
+    lat = soup.find('div', {'class': 'map-wrapper'}).find('div', {'id': 'map2gis'}).get('data-lat')
+    lon = soup.find('div', {'class': 'map-wrapper'}).find('div', {'id': 'map2gis'}).get('data-lon')
 
-    infos = soup.find('div', {'class':'details-main'}).find_all('div', {'class':'info-row'})
+    infos = soup.find('div', {'class': 'details-main'}).find_all('div', {'class': 'info-row'})
     add_info = {}
     for info in infos:
-        key = info.find('div', {'class':'label'}).text.strip()
-        value = info.find('div', {'class':'info'}).text.strip()
-        add_info.update({key:value})
-    print(title)
-    print(address)
-    print(price)
-    print(number)
-    print(f'широта - {lat}')
-    print(f'довгота - {lon}')
-    print(add_info['Дом'])
-    print('--------------')
+        key = info.find('div', {'class': 'label'}).text.strip()
+        value = info.find('div', {'class': 'info'}).text.strip()
+        add_info.update({key: value})
+    # print(title)
+    # print(address)
+    # print(price)
+
+    # array.append(number)
+    # print(f'широта - {lat}')
+    # print(f'довгота - {lon}')
+    # print(add_info['Дом'])
+    data = {
+        "number":number,
+        "lat":lat,
+        "lon":lon,
+    }
+    return data
+
+
+def get_last_page(html):
+    soup = BS(html, "html.parser")
+    ul_pagination = soup.find('ul', {'class': 'pagination'})
+    list_li = ul_pagination.find_all('li', {'class': 'page-item'})
+    last_btn = list_li[-1]
+    last_page_number = int(last_btn.find('a').get('data-page'))
+    return last_page_number
+
+
+def write_excel(data, row_number):
+    workbook = xlsxwriter.Workbook('house_kg.xlsx', options={"in_m"})
+    worksheet = workbook.add_worksheet()
+    col=0
+    for value in data.values():
+        worksheet.write(row_number, col, value)
+        col+=1
+    workbook.close()
 
 
 
 def main():
-    html = get_html(URL)
-    links = get_post_links(html)
-    for link in links:
-        detail_html = get_html(link)
-        get_post_data(detail_html)
+    time_start = datetime.now()
+    URL = 'https://www.house.kg/snyat-kvartiru?region=1&town=2&sort_by=upped_at+desc'
+    last_page_number = get_last_page(get_html(URL))
+    row_number = 0
+    for i in range(1, 2):
+        page_url = URL + f'&page{i}'
+        # print(page_url)
+        html = get_html(page_url)
+        links = get_post_links(html)
+        for link in links:
+            detail_html = get_html(link)
+            data = get_post_data(detail_html)
+            write_excel(data=data, row_number=row_number)
+            row_number+=1
+    time_end = datetime.now()
+    print(time_end-time_start)
 
 
 if __name__ == '__main__':
     main()
+
+# print(array)
